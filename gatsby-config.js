23e3f243f545b5aebf82c78b8ee38d76ebc62031
Eufronio <eufroniowoori@gmail.com>
2021-10-14 13:23:01 -0700

    t4

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `hdez`,
+      twitter: `Jahnhdez`,
     },
   },
   plugins: [
